cab6c67de687029b827712fea96432b95b0a964c
Re11037-00 <santos.rebeka@escolar.ifrn.edu.br>
2026-01-29 18:13:21 -0300

    botões de ESC e ENTER funcionais

--- jogo.py
+++ jogo.py
@@ -5,15 +5,28 @@ screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 running = True
 
+estado = 'menu'
+personagem = 0
+
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  screen.fill((255, 255, 255))
+    elif event.type == pygame.KEYDOWN:  
+
+        if event.key == pygame.K_ESCAPE:
+            running = False  #jogo fechará se apertar esc
+
+        elif estado == 'menu' and event.key == pygame.K_RETURN:
+            estado = 'selecao_personagem' #jogo abrirá menu de seleção se apertar enter
+        
+
+        if estado == 'menu':
+            screen.fill((255, 255, 255))
+    
 
   pygame.display.flip()
-
   clock.tick(60)
 
 pygame.quit()
