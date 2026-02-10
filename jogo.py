48b6644786657b799e76212c827202b67d894566
Re11037-00 <santos.rebeka@escolar.ifrn.edu.br>
2026-02-09 22:07:20 -0300

    correção de códigos + novos fundos

--- jogo.py
+++ jogo.py
@@ -10,28 +10,51 @@ font = pygame.font.Font(None, 36)
 #variaveis
 estado = 'menu'
 personagem = 0
+
 opcoes = ['pedra', 'papel', 'tesoura']
 escolha_jogador = None
-escolha_computador = random.choice(opcoes)
+escolha_computador = None
+
 pontos_jogador = 5
 pontos_computador = 5
-resultado = 0
+
+rodada_ativa = False
 
 #balão de fala
-mensagem = ""
-texto = font.render(mensagem, True, (255, 255, 255))
+linha1 = '' #pontuação
+linha2 = '' #jogador
+linha3 = '' #computador
+linha4 = '' #resultado
 
 # fundos 
-tela_inicial = pygame.image.load('assets/tela_inicial.jpeg').convert()  # Imagem de tela inicial
+
+# Imagem de tela inicial
+tela_inicial = pygame.image.load('assets/tela_inicial.jpeg').convert()  
 tela_inicial = pygame.transform.scale(tela_inicial, (640, 480))
-tela_selecao_p1 = pygame.image.load('assets/p1_selecionado.jpeg').convert()  # Seleção P1
+
+# Seleção P1
+tela_selecao_p1 = pygame.image.load('assets/p1_selecionado.jpeg').convert()  
 tela_selecao_p1 = pygame.transform.scale(tela_selecao_p1, (640, 480))
-tela_selecao_p2 = pygame.image.load('assets/p2_selecionado.jpeg').convert()  # Seleção P2
+
+ # Seleção P2
+tela_selecao_p2 = pygame.image.load('assets/p2_selecionado.jpeg').convert() 
 tela_selecao_p2 = pygame.transform.scale(tela_selecao_p2, (640, 480))
-bg_jogo_p1 = pygame.image.load('assets/p2_oponente.jpeg').convert()  # P2 como oponente
+
+# P2 como oponente
+bg_jogo_p1 = pygame.image.load('assets/p2_oponente.jpeg').convert()  
 bg_jogo_p1 = pygame.transform.scale(bg_jogo_p1, (640, 480))
-bg_jogo_p2 = pygame.image.load('assets/p1_oponente.jpeg').convert()  # P1 como oponente
+
+# P1 como oponente
+bg_jogo_p2 = pygame.image.load('assets/p1_oponente.jpeg').convert()  
 bg_jogo_p2 = pygame.transform.scale(bg_jogo_p2, (640, 480))
+
+#vitoria
+vitoria = pygame.image.load('assets/vitoria.png').convert()
+vitoria = pygame.transform.scale(vitoria, (640, 480))
+
+#derrota
+derrota = pygame.image.load('assets/derrota.png').convert()
+derrota = pygame.transform.scale(derrota, (640, 480))
 
 
 while running:
@@ -41,22 +64,29 @@ while running:
 
     elif event.type == pygame.KEYDOWN:  
 
+    #jogo fechará se apertar esc
         if event.key == pygame.K_ESCAPE:
-            running = False  #jogo fechará se apertar esc
+            running = False  
 
+    #jogo abrirá menu de seleção se apertar enter
         elif estado == 'menu' and event.key == pygame.K_RETURN:
-            estado = 'selecao_personagem' #jogo abrirá menu de seleção se apertar enter
+            estado = 'selecao_personagem' 
 
+    #seleção de personagem
         elif estado == 'selecao_personagem':
             if event.key == pygame.K_LEFT:
                 personagem = 0 #personagem da esquerda
             elif event.key == pygame.K_RIGHT:
                 personagem = 1 #personagem da direita
 
-            elif estado == 'selecao_personagem' and event.key == pygame.K_RETURN:
-                estado = 'jogo'  #inicia o jogo
+    #inicia o jogo
+        elif estado == 'selecao_personagem' and event.key == pygame.K_RETURN:
+                estado = 'jogo'  
         
-            if estado == 'jogo': #controles do jogo
+    #controles + lógica do jogo
+        elif estado == 'jogo': 
+
+
                 if event.key == pygame.K_LEFT:
                     escolha_jogador = 'pedra'
                 elif event.key == pygame.K_UP:
@@ -87,24 +117,25 @@ while running:
             mensagem = ""
 
         # Definir fundo
+    
+    #definir tela
     if estado == 'menu':
         screen.blit(tela_inicial, (0, 0))
+
     elif estado == 'selecao_personagem':
         if personagem == 0:
             screen.blit(tela_selecao_p1, (0, 0))
         elif personagem == 1:
             screen.blit(tela_selecao_p2, (0, 0))
+
     elif estado == 'jogo':
         if personagem == 0:
             screen.blit(bg_jogo_p1, (0, 0))
-        else:
+        if personagem == 1:
             screen.blit(bg_jogo_p2, (0, 0))
 
-    #criar balão
-    if estado == 'jogo':
-        # texto = font.render(mensagem, True, (255, 255, 255))
-        pygame.draw.rect(screen, (0, 0, 0), (20, 350, 600, 100))
-        screen.blit(texto, (30, 360))
+
+    
 
 
   pygame.display.flip()
